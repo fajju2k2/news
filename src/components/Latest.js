c7b5d3b478c0994abf805c5597f0f209020d16ca
fajju2k2 <64869093+fajju2k2@users.noreply.github.com>
2022-12-08 10:33:55 +0500

    workingbackend

--- src/components/Latest.js
+++ src/components/Latest.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import Navbar from './Navbar';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Latest = () => {
+  const [news, setNews] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/latest');
+      setNews(result);
+    };
+    fetchData();
+  }, []);
   return (
     <>
       {<Navbar />}
+
       <section className="hero-section">
         <p> Latest </p>
         <h1> News </h1>
